07bb251745e4dc208ed8dd504d02a3e1b392b055
nikitazh12 <nikitazh12@gmail.com>
2026-03-20 22:49:30 +0300

    step 6

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -2,10 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite',
-  },
-  production: {
     dialect: 'postgres',
     database: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
@@ -14,15 +10,11 @@ module.exports = {
     host: process.env.DATABASE_HOST,
   },
   test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
   },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
 };
